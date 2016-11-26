e8ba9f187d0d89a564bbe58e3aaf5e8b2fbba0ef
fabienf <fabien.florek@gmail.com>
2016-11-26 03:59:59 +0200

    removed loc server calls

--- messages/index.js
+++ messages/index.js
@@ -21,7 +21,7 @@ var chatConnector = new builder.ChatConnector({
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var chatBot = new builder.UniversalBot(chatConnector);
-server.post('/api/messages', chatConnector.listen());
+/*server.post('/api/messages', chatConnector.listen());
 
 // Create calling bot
 var connector = new calling.CallConnector({
@@ -31,7 +31,8 @@ var connector = new calling.CallConnector({
 });
 var bot = new calling.UniversalCallBot(connector);
 server.post('/api/calls', connector.listen());
-
+*/
+var bot = new calling.UniversalCallBot(connector);
 //=========================================================
 // Chat Dialogs
 //=========================================================
